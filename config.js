e4bcda2c1f1f045a3bcecff69e1b4b86a00ca03c
Dok <ddumansky@gmail.com>
2014-11-17 19:22:31 +0200

    added config file with DB URLs

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Dok on 11/17/2014.
+ */
+module.exports = {
+    db: {
+        production: "mongodb://adflowgic:adflowgic@dogen.mongohq.com:10090/adflowgic",
+        development: "mongodb://localhost:27017/adflowgic",
+        test: "mongodb://localhost:27017/adflowgic-test"
+    }
+};
