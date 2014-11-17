cf35637f08cd5935cf5c6884dfa817d880d96cf1
Dok <ddumansky@gmail.com>
2014-11-17 07:02:49 +0200

    initial schema

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
+
+// Database
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/adflowgic");
+
+//Require all the models
+fs.readdirSync(__dirname + '/models').forEach(function(filename){
+    if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename);
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var  advertisers = require('./routes/advertisers');
+var  networks= require('./routes/networks');
+var  media= require('./routes/media');
+var  media_containers= require('./routes/media_containers');
+var  layouts= require('./routes/layouts');
+var  broadcast_logs= require('./routes/broadcast_logs');
+var  schedules= require('./routes/schedules');
+var  events = require('./routes/events');
+
+
 
 var app = express();
 
@@ -22,8 +44,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+/*app.use(function(req,res,next){
+    req.db = db;
+    next();
+});*/
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/advertisers', advertisers);
+app.use('/networks', networks);
+app.use('/events', events);
+app.use('/media', media);
+app.use('/media_containers', media_containers);
+app.use('/layouts', layouts);
+app.use('/broadcast_logs', broadcast_logs);
+app.use('/schedules', schedules);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
