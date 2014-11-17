b410b67cb7b3ad776cf3773fa737c8d705125a01
Dok <ddumansky@gmail.com>
2014-11-17 07:58:43 +0200

    DB connection config

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 
 // Database
 //var mongoose = require('mongoose');
-//mongoose.connect("mongodb://ddumansky@gmail.com:xVpsyh4f@dogen.mongohq.com:10090/adflowgic");
+mongoose.connect("mongodb://adflowgic:adflowgic@dogen.mongohq.com:10090/adflowgic");
 
 
 //Require all the models
